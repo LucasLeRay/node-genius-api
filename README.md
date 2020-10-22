719b6074c893a2e7141fd0dc04669027645c3ece
LucasLeRay <contact@lucas-le-ray.com>
2020-10-22 23:17:58 +0200

    feat: add doc and readme

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# node-genius-api
+# Genius API Node client
+
+## Setup
+
+```bash
+npm install node-genius-api
+
+# Or with yarn
+yarn add node-genius-api
+```
+
+## Initialization
+
+Instantiate Genius by calling the constructor with your `accessToken` (You can get it [here](https://genius.com/api-clients)).
+
+```javascript
+// Instantiate Genius
+const Genius = require('./index');
+const client = new Genius(process.env.GENIUS_ACCESS_TOKEN);
+
+// Get infos of song with ID=378195
+client.song('378195').then((song) => {
+  console.log('The song is:', song.full_title)
+}).catch(console.error);
+```
+
+## Examples
+
+```javascript
+// Get data for a specific song.
+client.song('378195').then(console.log).catch(console.error);
+
+// Get lyrics of a specific song.
+client.lyrics('378195').then(console.log).catch(console.error);
+
+// Search songs.
+client.search('Redemption song').then(console.log).catch(console.error);
+
+// Get data for a specific artist.
+client.artist('16775').then(console.log).catch(console.error);
+
+// Get songs of an artist.
+client.artistSongs('16775', {
+  sort: 'popularity',
+  count: 20,
+  page: 1
+}).then(console.log).catch(console.error);
+
+// Get referents by content item or user responsible for an included annotation.
+client.referents({
+  webPage: '10347',
+  format: 'plain'
+}).then(console.log).catch(console.error);
+
+// Get data for a specific annotation.
+client.annotation('10225840', 'plain').then(console.log).catch(console.error);
+```
--- index.js
+++ index.js
@@ -12,10 +12,17 @@ function handleError(err) {
   }
 }
 
+/**
+ * Genius client
+ */
 module.exports = class Genius {
-  constructor({ accessToken }) {
+  /**
+   * Initialize Genius
+   * @param {string} accessToken - Genius API access token.
+   */
+  constructor(accessToken) {
     if (!accessToken) throw new Error('No Genius API access token provided');
-    this.instance = axios.create({
+    this.API = axios.create({
       baseURL: 'https://api.genius.com',
       headers: {
         common: {
@@ -23,19 +30,30 @@ module.exports = class Genius {
         },
       },
     });
+    this.WEB = axios.create({
+      baseURL: 'https://genius.com',
+    });
   }
 
+  /**
+   * Data for a specific song.
+   * @param {string} id - ID of the song.
+   */
   song(id) {
     return new Promise((resolve, reject) => {
-      this.instance.get(`/songs/${id}`).then((res) => {
+      this.API.get(`/songs/${id}`).then((res) => {
         resolve(res.data.response.song);
       }).catch((err) => reject(handleError(err)));
     });
   }
 
+  /**
+   * Search songs hosted on Genius.
+   * @param {string} q - The term to search for.
+   */
   search(q) {
     return new Promise((resolve, reject) => {
-      this.instance.get('/search', {
+      this.API.get('/search', {
         params: { q },
       }).then((res) => {
         resolve(res.data.response.hits);
@@ -43,17 +61,29 @@ module.exports = class Genius {
     });
   }
 
+  /**
+   * Data for a specific artist.
+   * @param {string} id - ID of the artist.
+   */
   artist(id) {
     return new Promise((resolve, reject) => {
-      this.instance.get(`/artists/${id}`).then((res) => {
+      this.API.get(`/artists/${id}`).then((res) => {
         resolve(res.data.response.artist);
       }).catch((err) => reject(handleError(err)));
     });
   }
 
+  /**
+   * Songs for the artist specified.
+   * @param {string} id - ID of the artist.
+   * @param {Object} [options] - Options for the query.
+   * @param {string} [options.sort=title] - Sort results (by title or popularity).
+   * @param {number} [options.count=20] - Number of results to return per request.
+   * @param {number} [options.page=1] - Paginated offset.
+   */
   artistSongs(id, { sort, count, page } = {}) {
     return new Promise((resolve, reject) => {
-      this.instance.get(`/artists/${id}/songs`, {
+      this.API.get(`/artists/${id}/songs`, {
         params: {
           sort,
           per_page: count,
@@ -65,14 +95,26 @@ module.exports = class Genius {
     });
   }
 
+  /**
+   * Referents by content item or user responsible for an included annotation.
+   * @param {Object} params - Params of the query.
+   * @param {string} [params.user] - ID of a user to get referents for.
+   * @param {string} [params.song] - ID of a song to get referents for.
+   * @param {string} [params.webPage] - ID of a web page to get referents for.
+   * @param {string} [params.format=dom] - Format for text bodies of documents. (dom, plain or html)
+   * @param {number} [params.count=20] - Number of results to return per request.
+   * @param {number} [params.page=1] - Paginated offset.
+   */
   referents({
     user, song, webPage, format, count, page,
   }) {
     return new Promise((resolve, reject) => {
       if (song && webPage) {
         reject(new Error('Cannot specify a song and a web page.'));
+      } else if (!song && !user && !webPage) {
+        reject(new Error('Must specify user ID, song ID or webPage ID.'));
       }
-      this.instance.get('/referents', {
+      this.API.get('/referents', {
         params: {
           created_by_id: user,
           song_id: song,
@@ -87,9 +129,14 @@ module.exports = class Genius {
     });
   }
 
+  /**
+   * Data for a specific annotation.
+   * @param {string} [id] - ID of the annotation.
+   * @param {string} [format=dom] - Format for text bodies of documents. (dom, plain or html)
+   */
   annotation(id, format) {
     return new Promise((resolve, reject) => {
-      this.instance.get(`/annotations/${id}`, {
+      this.API.get(`/annotations/${id}`, {
         params: {
           text_format: format,
         },
@@ -99,12 +146,15 @@ module.exports = class Genius {
     });
   }
 
-  // eslint-disable-next-line class-methods-use-this
+  /**
+   * Lyrics of a specific song.
+   * @param {string} [id] - ID of the song.
+   */
   async lyrics(id) {
     let lyrics;
     while (!lyrics) {
       // eslint-disable-next-line no-await-in-loop
-      const page = await axios.get(`https://genius.com/${id}`);
+      const page = await this.WEB.get(`/songs/${id}`);
       const $ = cheerio.load(page.data);
       lyrics = $('div[class="lyrics"]').text().trim();
     }
